5ed92b774943b3c141ee79783d695f116992e8c7
LoenK <leonkolloch@t-online.de>
2023-05-20 18:00:01 +0200

    ..

--- src/main/java/net/loenk/evertrieddarkarts/block/custom/HexBagBlock.java
+++ src/main/java/net/loenk/evertrieddarkarts/block/custom/HexBagBlock.java
@@ -6,29 +6,34 @@ import net.loenk.evertrieddarkarts.util.HexBagIdAndPowerManager;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.particle.ParticleTypes;
 
+import net.minecraft.predicate.entity.EntityPredicates;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.ItemScatterer;
 
+import net.minecraft.util.TypeFilter;
 import net.minecraft.util.function.BooleanBiFunction;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.Random;
-
 
 
 public class HexBagBlock extends BlockWithEntity {
@@ -39,6 +44,21 @@ public class HexBagBlock extends BlockWithEntity {
         super(settings);
     }
 
+
+    @Override
+    public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        BlockEntity blockEntity = world.getBlockEntity(pos);
+        HexBagBlockEntity hexBagBlockEntity = (HexBagBlockEntity) blockEntity;
+        if (hexBagBlockEntity.SpellID != 0) {
+            switch (hexBagBlockEntity.SpellID) {
+                case HexBagIdAndPowerManager.HEALING_SPELL:
+                    applyStatusEffectToEntitiesInRange(world, pos, 3,new StatusEffectInstance(StatusEffects.REGENERATION, 200, hexBagBlockEntity.SpellPower - 1));
+                    break;
+            }
+        }
+
+        super.scheduledTick(state, world, pos, random);
+    }
 
     @Override
     public void onPlaced(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack itemStack) {
@@ -92,9 +112,6 @@ public class HexBagBlock extends BlockWithEntity {
     }
 
 
-    int getSpellPower(HexBagBlockEntity hexBagBlockEntity) {
-        return 0;
-    }
 
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
@@ -104,19 +121,40 @@ public class HexBagBlock extends BlockWithEntity {
             return ActionResult.SUCCESS;
         }
 
-
         BlockEntity blockEntity = world.getBlockEntity(pos);
         player.sendMessage(new LiteralText("HexBagelSpellID: " + ((HexBagBlockEntity)blockEntity).SpellID), false);
         player.sendMessage(new LiteralText("HexBagelSpellPower: " + ((HexBagBlockEntity)blockEntity).SpellPower), false);
 
         if (blockEntity instanceof  HexBagBlockEntity) {
             player.openHandledScreen((HexBagBlockEntity)blockEntity);
+
+            // REMOVE
+            HexBagBlockEntity hexBagBlockEntity = (HexBagBlockEntity) blockEntity;
+            if (hexBagBlockEntity.SpellID != 0) {
+                switch (hexBagBlockEntity.SpellID) {
+                    case HexBagIdAndPowerManager.HEALING_SPELL:
+                        player.sendMessage(new LiteralText("cock"), false);
+                        applyStatusEffectToEntitiesInRange(world, pos, 3,new StatusEffectInstance(StatusEffects.REGENERATION, 200, hexBagBlockEntity.SpellPower - 1));
+                        break;
+                }
+            }
         }
 
 
         return ActionResult.CONSUME;
     }
 
+    void applyStatusEffectToEntitiesInRange(World world, BlockPos pos, float maxDistance, StatusEffectInstance effect) {
+        TypeFilter filter = TypeFilter.instanceOf(LivingEntity.class);
+        Box box = new Box(pos.getX()-maxDistance,pos.getY()-maxDistance,pos.getZ()-maxDistance,pos.getX()+maxDistance,pos.getY()+maxDistance,pos.getZ()+maxDistance);
+        List<LivingEntity> entities = world.getEntitiesByType(filter, box, EntityPredicates.VALID_LIVING_ENTITY);
+
+        for (LivingEntity livingEntity: entities) {
+            livingEntity.addStatusEffect(effect);
+        }
+
+
+    }
     @Override
     public BlockRenderType getRenderType(BlockState state) {
         return BlockRenderType.MODEL;
